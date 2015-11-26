a6a5998b3876b1954659435a1da6ea0d62dd2fb6
Jacob <jacob.simon01@gmail.com>
2015-11-26 12:08:21 -0500

    repository with basic structure

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.use(express.static(__dirname+'/js'));
+app.use(express.static(__dirname+'/css'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var server = app.listen(app.get('port'), function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Airports web server listening at http://%s:%s', host, port);
+});
